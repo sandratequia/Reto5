35bb56e41e3af78d2dc6dfbdf5c4c23e0287d4a5
“sandratequiagit config --global user.name “sandratequia <sandra.tequia.mt@usa.edu.co>
2022-10-11 19:44:51 -0500

    first commit

---
+++ src/main/java/com/example/demo/Repositorio/CountClient.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.Repositorio;
+
+import com.example.demo.Modelo.Client;
+
+/**
+ *
+ * @author s_a_w
+ */
+public class CountClient {
+    
+    private Long total;
+    private Client client;
+
+    public CountClient(Long total, Client client) {
+        this.total = total;
+        this.client = client;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+    
+}
---
+++ src/main/java/com/example/demo/Servicio/Status.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.Servicio;
+
+/**
+ *
+ * @author s_a_w
+ */
+public class Status {
+    private int completed;
+    private int cancelled;
+
+    public Status(int completed, int cancelled) {
+        this.completed = completed;
+        this.cancelled = cancelled;
+    }
+
+    public int getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(int completed) {
+        this.completed = completed;
+    }
+
+    public int getCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(int cancelled) {
+        this.cancelled = cancelled;
+    }
+    
+    
+}
